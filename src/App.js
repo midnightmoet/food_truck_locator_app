1b7a2926d486fee2063f21c41524bc994699a5fe
Lisa Miner <midnightmoet@gmail.com>
2023-05-15 19:02:49 -0400

    Adding elements that may or not make the final cut

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from './constants/footer/Footer';
 import Header from './constants/header/Header';
 import NavBar from './constants/navbar/NavBar';
-import Map from './pages/map/Map';
+import Map from './constants/map/Map';
 
 const App = () => {
   return (
