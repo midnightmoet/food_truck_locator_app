23589ab13c72e4dd660ccd97fffae2dae8984441
Lisa Miner <midnightmoet@gmail.com>
2023-05-18 17:13:51 -0400

    Changed some text on the searchbox

--- src/constants/searchbox/SearchBox.jsx
+++ src/constants/searchbox/SearchBox.jsx
@@ -7,14 +7,12 @@ const SearchBar = () => {
       <div className="form-container">
         <h1>Food Truck Finder</h1>
         <p>
-          "The best app to find food trucks near you!"
-          <br></br>
-          "Find them in two ways:"
+          The best app to find food trucks near you! Find them in two ways:
         </p>
         <p>1. Entering your current address to see nearby food trucks.</p>
         <p>2. Click anywhere on the map to update your current location.</p>
         <form>
-          <input type="text" placeholder="Enter your address"></input>
+          <input type="text" placeholder="Enter your address" required></input>
           <button type="submit">Find Food Truck</button>
           <button type="submit"><a href="/signup">Save to Favorites</a></button>
         </form>
